a383e5bd5da3000edc865637a69b923ceebf2ce7
Shohruhbek <shohruhbekaxmadjonov2004@gmail.com>
2023-06-17 15:35:32 +0500

    Updated code

--- store/urls.py
+++ store/urls.py
@@ -7,50 +7,50 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('profile/', views.profile, name='profile'),
-#     path('register/', views.register, name='register'),
-#     path('login/', views.login_view, name='login_view'),
-#     path('logout/', views.logout_view, name='logout'),
-#     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-#     path('category/<int:pk>/', views.category_detail, name='category_detail'),
-#     path('product/<int:pk>/order/', views.place_order, name='place_order'),
-#     path('cart/', views.cart, name='cart'),
-#     path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
-#     path('remove-from-cart/<int:pk>/', views.remove_from_cart, name='remove_from_cart'),
-# ]
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('category/<int:pk>/', views.category_detail, name='category_detail'),
+    path('product/<int:pk>/order/', views.place_order, name='place_order'),
+    path('cart/', views.cart, name='cart'),
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:pk>/', views.remove_from_cart, name='remove_from_cart'),
+]
 
 # API
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Store API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
-urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/v1/profile/', views.ProfileView.as_view(), name='profile'),
-    path('api/v1/register/', views.RegisterView.as_view(), name='register'),
-    path('api/v1/login/', auth_token_views.obtain_auth_token, name='login_view'),
-    path('api/v1/logout/', views.logout_view, name='logout'),
-    path('api/v1/product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('api/v1/category/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
-    path('api/v1/product/<int:pk>/order/', views.PlaceOrderView.as_view(), name='place_order'),
-    path('api/v1/cart/', views.CartView.as_view(), name='cart'),
-    path('api/v1/add-to-cart/<int:pk>/', views.AddToCartView.as_view(), name='add_to_cart'),
-    path('api/v1/remove-from-cart/<int:pk>/', views.RemoveFromCartView.as_view(), name='remove_from_cart'),
-]
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Store API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=[permissions.AllowAny],
+# )
+#
+# urlpatterns = [
+#     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#     path('api/v1/profile/', views.ProfileView.as_view(), name='profile'),
+#     path('api/v1/register/', views.RegisterView.as_view(), name='register'),
+#     path('api/v1/login/', auth_token_views.obtain_auth_token, name='login_view'),
+#     path('api/v1/logout/', views.logout_view, name='logout'),
+#     path('api/v1/product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+#     path('api/v1/category/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
+#     path('api/v1/product/<int:pk>/order/', views.PlaceOrderView.as_view(), name='place_order'),
+#     path('api/v1/cart/', views.CartView.as_view(), name='cart'),
+#     path('api/v1/add-to-cart/<int:pk>/', views.AddToCartView.as_view(), name='add_to_cart'),
+#     path('api/v1/remove-from-cart/<int:pk>/', views.RemoveFromCartView.as_view(), name='remove_from_cart'),
+# ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -27,108 +27,108 @@ def logout_view(request):
     return redirect('home')
 
 
-# -----------------
-# def profile(request):
-#     user = request.user
-#     return render(request, 'profile.html', {'user': user})
-#
-#
-# def register(request):
-#     if request.method == 'POST':
-#         form = CustomUserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form = CustomUserCreationForm()
-#     return render(request, 'register.html', {'form': form})
-#
-#
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('profile')
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'login.html', {'form': form})
-#
-#
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#
-#     if request.method == 'POST':
-#         if request.user.is_authenticated:
-#             count = int(request.POST['count'])
-#             # Calculate the total amount
-#             total_amount = count * float(product.price)
-#             return render(request, 'order.html', {'product': product, 'count': count, 'total_amount': total_amount})
-#         else:
-#             return redirect('register')
-#
-#     return render(request, 'product_detail.html', {'product': product})
-#
-#
-# def category_detail(request, pk):
-#     category = Category.objects.get(pk=pk)
-#     products = Product.objects.filter(category=category)
-#     return render(request, 'category_detail.html', {'category': category, 'products': products})
-#
-#
-# @login_required(login_url='login_view')
-# def add_to_cart(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     cart, created = Cart.objects.get_or_create(user=request.user)
-#     cart.products.add(product)
-#     return redirect('cart')
-#
-#
-# @login_required(login_url='login_view')
-# def remove_from_cart(request, pk):
-#     product = Product.objects.get(pk=pk)
-#     cart = Cart.objects.get(user=request.user)
-#     cart.products.remove(product)
-#     return redirect('cart')
-#
-#
-# @login_required(login_url='login_view')
-# def cart(request):
-#     cart, created = Cart.objects.get_or_create(user=request.user)
-#     products = cart.products.all()
-#     total_price = sum(product.price for product in products)
-#     product_quantities = cart.products.through.objects.filter(cart=cart).values('product').annotate(
-#         quantity=Count('product')).values('product', 'quantity')
-#
-#     product_quantity_map = {item['product']: item['quantity'] for item in product_quantities}
-#
-#     return render(request, 'cart.html', {'products': products, 'total_price': total_price, 'cart': cart,
-#                                          'product_quantity_map': product_quantity_map})
-#
-#
-# @login_required(login_url='login_view')
-# def place_order(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#
-#     if request.method == 'POST':
-#         count = int(request.POST['count'])
-#         total_amount = count * float(product.price)
-#
-#         # Create the order here
-#         order = Order.objects.create(
-#             product=product,
-#             user=request.user,
-#             count=count,
-#             total_amount=total_amount
-#         )
-#
-#         return render(request, 'order_placed.html', {'product': product, 'count': count, 'total_amount': total_amount})
-#
-#     return render(request, 'order_placed.html', {'product': product})
+def profile(request):
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            count = int(request.POST['count'])
+            # Calculate the total amount
+            total_amount = count * float(product.price)
+            return render(request, 'order.html', {'product': product, 'count': count, 'total_amount': total_amount})
+        else:
+            return redirect('register')
+
+    return render(request, 'product_detail.html', {'product': product})
+
+
+def category_detail(request, pk):
+    category = Category.objects.get(pk=pk)
+    products = Product.objects.filter(category=category)
+    return render(request, 'category_detail.html', {'category': category, 'products': products})
+
+
+@login_required(login_url='login_view')
+def add_to_cart(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart.products.add(product)
+    return redirect('cart')
+
+
+@login_required(login_url='login_view')
+def remove_from_cart(request, pk):
+    product = Product.objects.get(pk=pk)
+    cart = Cart.objects.get(user=request.user)
+    cart.products.remove(product)
+    return redirect('cart')
+
+
+@login_required(login_url='login_view')
+def cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    products = cart.products.all()
+    total_price = sum(product.price for product in products)
+    product_quantities = cart.products.through.objects.filter(cart=cart).values('product').annotate(
+        quantity=Count('product')).values('product', 'quantity')
+
+    product_quantity_map = {item['product']: item['quantity'] for item in product_quantities}
+
+    return render(request, 'cart.html', {'products': products, 'total_price': total_price, 'cart': cart,
+                                         'product_quantity_map': product_quantity_map})
+
+
+@login_required(login_url='login_view')
+def place_order(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        count = int(request.POST['count'])
+        total_amount = count * float(product.price)
+
+        # Create the order here
+        order = Order.objects.create(
+            product=product,
+            user=request.user,
+            count=count,
+            total_amount=total_amount
+        )
+
+        return render(request, 'order_placed.html', {'product': product, 'count': count, 'total_amount': total_amount})
+
+    return render(request, 'order_placed.html', {'product': product})
+
 
 # API
 
